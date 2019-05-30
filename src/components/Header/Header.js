95cfb8694e0bb63b48c4f8d6e6fc6b8721c61276
Erlend Solbakk Harbitz <eharbitz@gmail.com>
2019-05-30 23:14:54 +0200

    Working on header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,13 +3,42 @@ import { Link } from 'react-router-dom';
 import './header.css';
 
 class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+    this.handleScroll = this.handleScroll.bind(this);
+  }
+
+  handleScroll() {
+    this.setState({scroll: window.scrollY});
+  }
+
+  componentDidMount() {
+    const el = document.querySelector('nav');
+    this.setState({navHeight: el.offsetHeight});
+    window.addEventListener('scroll', this.handleScroll);
+  }
+
   render() {
     return (
       <div className="header">
-        <Link to="/Rank" rel="noopener noreferrer">Rank</Link>
-        <Link to="/About" rel="noopener noreferrer">About</Link>
-        <Link to="/Donations" rel="noopener noreferrer">Donations</Link>
-      </div>
+          <nav className={this.state.scroll > this.state.navHeight ? "fixed-nav" : ""}>
+            <ul>
+              <li>
+                <Link to="/" rel="noopener noreferrer">Monsrud Open</Link>
+              </li>
+              <li>
+                <Link to="/Rank" rel="noopener noreferrer">Rank</Link>
+              </li>
+              <li>
+                <Link to="/About" rel="noopener noreferrer">About</Link>
+              </li>
+              <li>
+                <Link to="/Donations" rel="noopener noreferrer">Donations</Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
     );
   }
 }
