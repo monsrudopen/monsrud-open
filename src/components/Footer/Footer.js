6ed233c67f8e521137546a3aa3fffef2650c69a9
Ferdinand Eide Rode <ferdi.rode@gmail.com>
2019-05-30 23:34:24 +0200

    Attempt of creating hovering pictures in footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,27 +1,24 @@
 import React, { Component } from 'react';
 import './Footer.css';
+import instaBW from '../src/img/icons/instaBW.png';
 
 class Footer extends Component {
   render() {
     return (
       <div>
+        <p>Monsrud Open &copy; {new Date().getFullYear()}</p>
         <div className="box">
-          <div className="footer-left">
-            <p>Monsrud Open &copy; {new Date().getFullYear()}</p>
-            <p>Ruglandveien 107, 1359 Eiksmarka</p>
-            <p>Org.nr. 918 707 611</p>
-            <p>vipps: 109417</p>
+          <div>
+              <a href="mailto:hello@monsrudopen.com" target="_blank"><img className="footer-image" src="https://cdn4.iconfinder.com/data/icons/basic-ui-elements/700/08_mail-512.png" width="40"></img></a>
           </div>
-          <div className="footer-right">
-              <div>
-                <a href="mailto:hello@monsrudopen.com" target="_blank"><img className="footer-image" src="https://cdn4.iconfinder.com/data/icons/basic-ui-elements/700/08_mail-512.png" width="40"></img></a>
-              </div>
-              <div>
+          <div>
                 <a href="www.twitter.com/monsrudopen" target="_blank"><img className="footer-image" src="https://image.flaticon.com/icons/svg/54/54300.svg" width="40"></img></a>
-              </div>
-              <div>
-                <a href="www.instagram.com/monsrudopen" target="_blank"><img className="footer-image" src="https://ui-ex.com/images250_/instagram-transparent-circular-4.png" width="40"></img></a>
-              </div>
+          </div>
+          <div>
+              <a href="www.instagram.com/monsrudopen" target="_blank"><img className="footer-image" src="https://ui-ex.com/images250_/instagram-transparent-circular-4.png" width="40"></img></a>
+          </div>
+          <div>
+          <img className="footer-image" alt="InstaBW" src="instaBW" width="40"></img>
           </div>
         </div>
       </div>
