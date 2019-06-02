d847d912190a9bf6b562582921df6348f5ec2d19
Ferdinand Eide Rode <ferdi.rode@gmail.com>
2019-06-02 14:55:24 +0200

    countdown finished

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -16,27 +16,37 @@ class Footer extends Component {
         <div className="box">
           <div>
             <a href="mailto:hello@monsrudopen.com" target="_blank">
-            <HoverImage src={MailBW} hoverSrc={MailC} style={{width: 40}}>
-            </HoverImage>
+              <HoverImage src={MailBW} hoverSrc={MailC} style={{ width: 40 }} />
             </a>
           </div>
           <div>
             <a href="https://www.twitter.com/monsrudopen" target="_blank">
-            <HoverImage src={TwitterBW} hoverSrc={TwitterC} style={{width: 40}}>
-            </HoverImage>
+              <HoverImage
+                src={TwitterBW}
+                hoverSrc={TwitterC}
+                style={{ width: 40 }}
+              />
             </a>
           </div>
           <div>
             <a href="https://www.instagram.com/monsrudopen" target="_blank">
-              <HoverImage className="image" src={InstaBW} hoverSrc={InstaC} style={{width: 40}}>
-              </HoverImage>
+              <HoverImage
+                className="image"
+                src={InstaBW}
+                hoverSrc={InstaC}
+                style={{ width: 40 }}
+              />
             </a>
           </div>
-          </div>
-            <a href="https://goo.gl/maps/udc6qp4q9tMbrJYB7" target="_blank"><p>Ruglandveien 107, 1359 Eiksmarka</p></a>
-            <p>Org.nr.: 918 707 611</p>
-            <p>Vipps: 109417</p>
         </div>
+        <p>
+          <a href="https://goo.gl/maps/udc6qp4q9tMbrJYB7" target="_blank">
+            Ruglandveien 107, 1359 Eiksmarka
+          </a>
+        </p>
+        <p>Org.nr.: 918 707 611</p>
+        <p>Vipps: 109417</p>
+      </div>
     );
   }
 }
