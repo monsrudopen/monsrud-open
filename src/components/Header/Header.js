5a5644695e964b13fd06a5af7c98041bac0bbc35
Erlend Solbakk Harbitz <eharbitz@gmail.com>
2019-06-18 21:08:55 +0200

    small fixes

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -12,54 +12,56 @@ class Header extends Component {
 
   render() {
     return (
-      <div id="header" className="header">
-        <div className="headerContainer">
-          <div className="monsrudLogo">
-            <img
-              width="60"
-              height="60"
-              src={require('./../../img/icons/MonsrudLogo.png')}
-            />{' '}
-          </div>
-          <button className="hamburgerBar" onClick={this.toggleMenu}>
-            Menu
-          </button>
-          <div className="menuList">
-            <div className="menuLink">
-              <Link
-                style={{ color: 'white', textDecoration: 'none' }}
-                to="/"
-                rel="noopener noreferrer"
-              >
-                <h6>Monsrud Open</h6>
-              </Link>
+      <div className="header-padding">
+        <div id="header" className="header">
+          <div className="headerContainer">
+            <div className="monsrudLogo">
+              <img
+                width="60"
+                height="60"
+                src={require('./../../img/icons/MonsrudLogo.png')}
+              />{' '}
             </div>
-            <div className="menuLink">
-              <Link
-                style={{ color: 'white', textDecoration: 'none' }}
-                to="/Rank"
-                rel="noopener noreferrer"
-              >
-                <h6>Ranking</h6>
-              </Link>
-            </div>
-            <div className="menuLink">
-              <Link
-                style={{ color: 'white', textDecoration: 'none' }}
-                to="/Donations"
-                rel="noopener noreferrer"
-              >
-                <h6>Tidligere donasjoner</h6>
-              </Link>
-            </div>
-            <div className="menuLink">
-              <Link
-                style={{ color: 'white', textDecoration: 'none' }}
-                to="/About"
-                rel="noopener noreferrer"
-              >
-                <h6>Om oss</h6>
-              </Link>
+            <button className="hamburgerBar" onClick={this.toggleMenu}>
+              Menu
+            </button>
+            <div className="menuList">
+              <div className="menuLink">
+                <Link
+                  style={{ color: 'white', textDecoration: 'none' }}
+                  to="/"
+                  rel="noopener noreferrer"
+                >
+                  <h6>Monsrud Open</h6>
+                </Link>
+              </div>
+              <div className="menuLink">
+                <Link
+                  style={{ color: 'white', textDecoration: 'none' }}
+                  to="/Rank"
+                  rel="noopener noreferrer"
+                >
+                  <h6>Ranking</h6>
+                </Link>
+              </div>
+              <div className="menuLink">
+                <Link
+                  style={{ color: 'white', textDecoration: 'none' }}
+                  to="/Donations"
+                  rel="noopener noreferrer"
+                >
+                  <h6>Tidligere donasjoner</h6>
+                </Link>
+              </div>
+              <div className="menuLink">
+                <Link
+                  style={{ color: 'white', textDecoration: 'none' }}
+                  to="/About"
+                  rel="noopener noreferrer"
+                >
+                  <h6>Om oss</h6>
+                </Link>
+              </div>
             </div>
           </div>
         </div>
