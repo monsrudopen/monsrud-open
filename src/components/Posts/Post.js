382ee96d9d58cecb78595c011c3e40be165a5b05
Erlend Solbakk Harbitz <erlend.solbakk.harbitz@sintef.no>
2020-04-07 18:47:53 +0200

    testing by removing serve package

--- src/components/Posts/Post.js
+++ src/components/Posts/Post.js
@@ -1,11 +1,11 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import './posts.css';
 
 function Post(props) {
   return (
     <a className="post" href={props.link}>
-      <img className="postImage" src={props.image} />
+      <img alt={props.title} className="postImage" src={props.image} />
       <div className="postTitle">{props.title}</div>
       <div className="postTexter">{props.text}</div>
     </a>
