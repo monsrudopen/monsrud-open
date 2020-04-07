b73c6d5d428d6f3b52a7780cad0953ef7fae8f66
Erlend Solbakk Harbitz <erlend.solbakk.harbitz@sintef.no>
2020-04-07 12:24:46 +0200

    fix bug

--- src/components/Competitions/Competitions.js
+++ src/components/Competitions/Competitions.js
@@ -1,13 +1,8 @@
 import React, { Component } from 'react';
-import Tournament from '../Tournament/Tournament';
 
 class Competitions extends Component {
   render() {
-    return (
-      <div className="Main">
-        <Tournament />
-      </div>
-    );
+    return <div className="Main" />;
   }
 }
 
