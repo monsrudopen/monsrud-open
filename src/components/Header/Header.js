b64a50473a109c505ca4c7e5bee06e0d5cc9d7c7
Erlend Solbakk Harbitz <erlend.solbakk.harbitz@sintef.no>
2020-04-11 19:07:31 +0200

    remove warnings

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import styles from './Header.module.css';
-import DonateButton from '../DonateButton/DonateButton';
+// import DonateButton from '../DonateButton/DonateButton';
 
 class Header extends Component {
   constructor(props) {
