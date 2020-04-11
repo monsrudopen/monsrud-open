df292c4dcd96324b6993509910ef8b6201e794d4
Ferdinand Eide Rode <ferdi.rode@gmail.com>
2020-04-11 11:34:46 +0200

    Rettet opp tekster
    Rettet opp tekster i dataloader og endret slik at det er samme struktur for alle "sider" i .js.

--- src/database/dataloader.js
+++ src/database/dataloader.js
@@ -1,8 +1,10 @@
 const competitions = [
   {
     title: '2019 - Plans Jentefond',
-    organization: 'Plan Norge',
+    text:
+      'I 2019 gikk innsamlingen til Plan Norges jentefond og det ble samlet inn hele 17 701,08 kroner. ',
     winner: 'Ferdinand Eide Rode',
+    donated: 17701.08,
     attendees: [
       'Ferdinand Eide Rode',
       'Marius Monsrud',
@@ -33,17 +35,16 @@ const competitions = [
     ],
     tournamentText:
       'For tredje gang var finalen mellom Ferdinand Eide Rode og Marius Monsrud, hvor begge kunne bli mestvinnende sammen med Jakob Hardersen Prydz, med to seiere. Seirende ut gikk Ferdinand, som vant med 2-1 i sett etter å ha hatt 5 mesterskapsballer mot seg. Erlend Solbakk Harbitz gikk seirende ut av bronsefinalen mot Hanne L‘Orsa, som samtidig ble første kvinne til å spille i en semifinale, og bronsefinale, noen gang. Turneringen satt rekord i antall deltakere, på hele 26 stk. Logistikken ble satt på prøve, men med positive spillere og stab ordnet alt seg.',
-    donated: 17701.08,
-    text:
-      'I 2019 gikk innsamlingen til Plan Norges jentefond og det ble samlet inn hele 17 701,08 kroner. ',
+    organization: 'Plan Norge',
     organizationInfo:
       'I 2019 gikk innsamlingen til Plan Norges jentefond og det ble samlet inn hele 17 701,08 kroner. Plan Norge jobber særlig med jenters rettigheter og likestilling. Jenter opplever diskriminering og urettferdighet i store deler av verden. Rundt 130 millioner jenter mellom 6 og 17 år går ikke på skolen i verden. Jenter faller fra, særlig i tenårene, fordi familien ikke har råd eller fordi deres arbeidskraft trengs hjemme. Andre viktige årsaker er barneekteskap og tidlig graviditet. Dagens tempo må åttedobles skal FNs bærekraftsmål om likestilling og målet om avskaffelse av barneekteskap nås innen 2030. Plan gir barn og unge kunnskap om sine rettigheter, bygger skoler, jobber for å stoppe barneekteskap, skaper holdningsendringer og driver påvirkningsarbeid. Et eksempel på kunnskapsspredning kommer fra Malawi der gamle norske FM radioer deles for å enkelt kunne spre kunnskap. Pengene fra fondet har tidligere blitt brukt til å gjøre byen tryggere for jenter i India, kampen mote barneekteskap in Vietnam, i Zimbabwe har de jobbet for å øke jenters tilgang til utdanning, og de har arbeidet mot vold i skolen i Malawi.',
     image: require('./../img/images/y2019.jpg')
   },
   {
     title: '2018 - Redd Barna',
+    text:
+      'I 2018 samlet vi inn penger til Redd Barna. Tross noe færre deltakere enn året før klarte vi å samle inn kr 12 669.',
     winner: 'Marius Monsrud',
-    organization: 'Redd Barna',
     donated: 12669,
     attendees: [
       'Ferdinand Eide Rode',
@@ -61,18 +62,18 @@ const competitions = [
       'Hanne L‘Orsa',
       'Kristine Huseby Hübert'
     ],
-    text:
+    tournamenttext:
       'I 2018 vant Marius Monsrud etter at han fikk sin revansje mot Ferdinand Eide Rode i finalen med 2-1 seier i sett. Marius ble dermed første person til å ta med det nye vandretrofeet hjem, designet og laget av Sondre Angard Siljehaug, og var tydelig glad. I bronsefinalen ble det W.O. til førstegangs deltaker Thomas Dagsvik mot en annen førstegangs deltaker, Falk Eidsvold Tøien. Det var totalt 15 deltakere under turneringen i 2018.',
-    tournamentText:
-      'I 2018 var det endelig klart for hjemmeseier. Etter nok en tøff finale mot Ferdinand Eide Rode fikk han sin revansj. Etter to finaletap på rad var dette fortjent. Det skal nevnes at Marius hadde spilt badminton på NHH én gang i uken hele dette året og derfor kunne kalles favoritt. Noen gikk så langt som å kall det juks, men det stemmer ikke. Under Monsrud Open er det lov å være helt nybegynner, men det er også lov å være god! Ellers kunne Sondre Angard Siljehaug presentere et flokende nytt trofé. Dette flotte trofeet ble værende i Ruglandveien etter turneringen.',
+    organization: 'Redd Barna',
     organizationInfo:
       'I 2018 samlet vi inn penger til Redd Barna. Tross noe færre deltakere enn året før klarte vi å samle inn kr 12 669, noe vi var svært fornøyd med. For andre gang ønsket vi å sette fokus på barns rettigheter. I tiden før arrangementet ble barns rettigheter satt på prøve, og vestlige land sviktet. Skip med migranter som ønsket et bedre liv for seg og sin familie blir nektet adgang til Europeiske land. Trump satt barn i bur på grensen mellom USA og Mexico. 263 millioner barn er ikke i skole i dag. Redd Barna nådde nesten 10 millioner barn over hele verden i 2017 med sine utdanningsprogram. Redd Barna hjalp i alt i 2017 155 millioner barn i 120 forskjellige land. På grunn av dette var valget i 2018 enkelt. Redd Barna bygger alt sitt arbeid på barnekonvensjonen som skal sikre rettigheter til hvert enkelt barn. Redd Barna jobber for barns rettigheter i Norge, samt 120 andre land med temaer som utdanning, barns rett til helse, nødhjelp og barns beskyttelse. Monsrud Open er derfor svært stolt av vårt bidrag til Redd Barna i 2018',
     image: require('./../img/images/group2018.jpg')
   },
   {
     title: '2017 - Røde Kors',
+    text:
+      'I 2017 satt vi fokus på rent vann. Vi samlet inn fantastiske kr 14 756 til Røde Kors‘ vann for livet kampanje.',
     winner: 'Erlend Solbakk Harbitz',
-    organization: 'Røde Kors',
     donated: 14756,
     attendees: [
       'Erlend Solbakk Harbitz',
@@ -96,18 +97,18 @@ const competitions = [
       'Ingrid Nelson Giæver',
       'Vilde Senje'
     ],
-    text:
-      'I 2017 var det Erlend Solbakk Harbitz sin tur til å ta med vandretrofeet hjem etter seier i finalen mot Ferdinand Eide Rode som dermed gikk på sitt 3 finaletap på 4 år. Med sin første seier forbedret Erlend sin besteplassering, som tidligere var 4.plass. Dette året satte mørket tidsbegrensninger for bronsefinalen som gjorde at det ble en delt 3. plass mellom Jakob Hardersen Prydz og Sondre Angard Siljehaug. Turneringen talte utrolige 20 deltakere, noe som da var rekord for Monsrud Open. Årets store nyhet var svært flotte spillerkort laget av den kreative deltakeren Sondre Angard Siljehaug. Salg av disse bidro til økte inntekter til Røde Kors.',
     tournamentText:
-      '2017 var året for de store overraskelsene. Årets vinner var noe overraskende styremedlem Erlend Solbakk Harbitz. Marius Monsrud ble denne gangen igjen slått i finalen. Årets store nyhet var svært flotte spillerkort laget av vår kreative deltaker og venn Sondre Angard Siljehaug. Salg av disse bidro til økte inntekter til Røde Kors. Disse kortene vil trolig stige mye i verdi i årene fremover. 2017 var også året Monsrud Open ble en offisiell organisasjon etter at stiftelsesmøte var avholdt på Gløshaugen i Trondheim i starten av mars. Til stede var hele nåværende styre ved styreleder Marius Monsrud, Erlend Solbakk Harbitz, Ferdinand Eide Rode og Øystein Hammersland. Vedtekter ble skrevet og alle dokumenter ble sendt inn til Brønnøysundregisteret. Dette ga oss tilgang til egen nettbank og Vipps. Dette gjorde oss mer åpne.',
+      'I 2017 var det Erlend Solbakk Harbitz sin tur til å ta med vandretrofeet hjem etter seier i finalen mot Ferdinand Eide Rode som dermed gikk på sitt 3 finaletap på 4 år. Med sin første seier forbedret Erlend sin besteplassering, som tidligere var 4.plass. Dette året satte mørket tidsbegrensninger for bronsefinalen som gjorde at det ble en delt 3. plass mellom Jakob Hardersen Prydz og Sondre Angard Siljehaug. Turneringen talte utrolige 20 deltakere, noe som da var rekord for Monsrud Open. Årets store nyhet var svært flotte spillerkort laget av den kreative deltakeren Sondre Angard Siljehaug. Salg av disse bidro til økte inntekter til Røde Kors.',
+    organization: 'Røde Kors',
     organizationInfo:
       'I 2017 satt vi fokus på rent vann. Vi samlet inn fantastiske kroner 14 756 til Røde Kors til vann for livet kampanje. Dette ble da tredje året med en dobling av det innsamlede beløpet. Rent vann er et stort problem i mange deler av verden. Så mange som én milliard mennesker mangler tilgang til rent vann. Dette fører til at mer enn 6 000 mennesker dør daglig. De fleste av disse er barn. Tid som kunne vært brukt til å arbeide og skape en fremtid blir brukt til å hente vann. Det er ofte kvinner som må gå mange timer hver dag for å hente vann. Barn som skulle ha gått på skole kan også måtte slutte på skolen for å bidra til å hente rent vann. Dette skaper en ond sirkel av fattigdom for altfor mange. Røde Kors har prosjekter i Mosambik, Lesotho, Sudan, Rwanda, Kenya og Burundi. 1000 mennesker får rent vann ved å sette opp én brønn! Monsrud Open er stolte av å ha bidratt til dette arbeidet.',
     image: require('./../img/images/postImage3.png')
   },
   {
     title: '2016 - NRC',
+    text:
+      'I 2016 samlet vi inn kr 6 000 til Flyktninghjelpen. Igjen doblet vi beløpet til stor glede for alle sammen.',
     winner: 'Ferdinand Eide Rode',
-    organization: 'Flyktninghjelpen',
     donated: 6000,
     attendees: [
       'Andreas Dahlslett Ratvik',
@@ -123,18 +124,18 @@ const competitions = [
       'Thomas Matthew Brown',
       'Øystein Hammersland'
     ],
-    text:
-      'I 2016 vant Ferdinand Eide Rode for første gang turneringen, i fraværet av Jakob Hardersen Prydz. Ferdinand seiret med 2-0 i sett i finalen mot Marius Monsrud, som tidligere hadde 5. plass som beste plassering og gjorde dermed stor fremgang. Ferdinand revansjerte dermed sitt tap mot Marius fra gruppespillet. Thomas Matthew Brown gikk seirende ut av Bronsefinalen mot Erlend Solbakk Harbitz i en jevnt kamp som endte 2-1 i sett til Thomas. I 2016 så turneringen en liten reduksjon i antall deltakere, med totalt 12 spillere. Under selve arrangementet sto Kristine Hubert og Erik Bjørgum for fremragende underholdning.',
     tournamentText:
-      'Dette var året da den store favoritten, med mange års tenniserfaring endelig skulle lykkes. Ferdinand Eide Rode klarte endelig å hente hjem trofeet. Finalen var jevn og spennende mellom hjemmehåpet Marius Monsrud og Ferdinand. Etter tre jevne sett ble det hele avgjort. Ellers nytt av året var vårt flotte banner som har prydet bakgrunnen på de fleste bilder fra turneringen i etterkant. Styret fikk også flotte krus med logoen (selvfølgelig betalt av oss selv). Vi husker også den flotte promofilmen laget av Erlend Solbakk Harbitz som så flott satt sammen badminton og flyktninger. Under selve arrangementet sto Kristine Hubert og Erik Bjørgum for fremragende underholdning. Deres sang om Marius Monsrud på melodien til «kursiv» glemmes nok aldri. ',
+      'I 2016 vant Ferdinand Eide Rode for første gang turneringen, i fraværet av Jakob Hardersen Prydz. Ferdinand seiret med 2-0 i sett i finalen mot Marius Monsrud, som tidligere hadde 5. plass som beste plassering og gjorde dermed stor fremgang. Ferdinand revansjerte dermed sitt tap mot Marius fra gruppespillet. Thomas Matthew Brown gikk seirende ut av Bronsefinalen mot Erlend Solbakk Harbitz i en jevnt kamp som endte 2-1 i sett til Thomas. I 2016 så turneringen en liten reduksjon i antall deltakere, med totalt 12 spillere. Under selve arrangementet sto Kristine Hubert og Erik Bjørgum for fremragende underholdning.',
+    organization: 'Flyktninghjelpen',
     organizationInfo:
       'I 2016 samlet vi inn 6 000 kroner til Flyktninghjelpen. Igjen doblet vi beløpet til store glede for alle sammen. Flyktninghjelpen er Norges største humanitære organisasjon og de sørger for at flyktninger får mat, rent vann og et sted å sove. Av den 5 000 ansatte jobber størsteparten av dem lokalt i land ca. 25 land i Asia, Afrika og Sør-Amerika. Organisasjonen tilbyr beskyttelse og hjelp til mennesker på flukt. De jobber også mer langsiktig for flyktningers rettigheter og hjelper med juridiske problemer. 65,3 millioner mennesker var på flukt i 2015, en økning på nesten 6 millioner fra året før. Dette er det høyeste tallet som noen gang har vært registrert. Vi var derfor stolte over å kunne bidra til Flykningshjelpens arbeid akkurat dette året. I tillegg traff styreleder Marius Monsrud generalsekretær i Flyktninghjelpen Jan Egeland og fortalte han om innsamlingen. Han kunne fortelle at han aldri hadde hørt om en badmintoninnsamling før og at han satt stor pris på det vi hadde gjort.',
     image: require('./../img/images/y2016.jpeg')
   },
   {
     title: '2015 - Amnesty',
+    text:
+      'I 2015 samlet vi inn kr 3 000 til Amnesty. Dette var mer enn en dobling fra det første året noe vi var svært fornøyd med.',
     winner: 'Jakob Hardersen Prydz',
-    organization: 'Amnesty',
     donated: 3000,
     attendees: [
       'Erik Bjørgum',
@@ -154,17 +155,17 @@ const competitions = [
       'Thomas Matthew Brown'
     ],
     tournamentText:
-      'Antall spillere ble nesten doblet i 2015 til 15 deltakere, hvor det ble åpnet for kvinnelige deltakere også. Alle konkurrerte i åpen klasse, som igjen ble vunnet av Jakob Hardersen Prydz som sikret seg sin andre seier på like mange år.  Også i 2015 vant han finalen mot Ferdinand Eide Rode, men denne gangen med 2-0 i sett. Tredjeplassen gikk til Johannes Sand Bolstad som slo Thomas Brantsæter på w.o.Nytt av året var pauseunderholdning mellom gruppespill og sluttspill, hvor deltaker Kristine Huseby Hübert underholdt både spillere og tilskuere.',
-    text:
-      'I 2015 samlet vi inn kr 3 000 til Amnesty. Dette var mer enn en dobling fra det første året noe vi var svært fornøyd med.',
+      'Antall spillere ble nesten doblet i 2015 til 15 deltakere, hvor det ble åpnet for kvinnelige deltakere. Alle konkurrerte i åpen klasse, som igjen ble vunnet av Jakob Hardersen Prydz som sikret seg sin andre seier på like mange år.  Også i 2015 vant han finalen mot Ferdinand Eide Rode, men denne gangen med 2-0 i sett. Tredjeplassen gikk til Johannes Sand Bolstad som slo Thomas Brantsæter på w.o.Nytt av året var pauseunderholdning mellom gruppespill og sluttspill, hvor deltaker Kristine Huseby Hübert underholdt både spillere og tilskuere.',
+    organization: 'Amnesty',
     organizationInfo:
       'I 2015 samlet vi inn kr 3 000 til Amnesty. Dette var mer enn en dobling fra det første året noe vi var svært fornøyd med. Amnesty International er verdens største menneskerettighetsorganisasjon. De er en bevegelse som kjemper for ytringsfrihet og mot diskriminering og overgrep. De etterforsker og avslører brudd på menneskerettighetene, de aksjonerer for enkeltmennesker i fare, og for å skape varige endringer. Amnesty er uavhengig. Det gjør at de kan si sannheten om enhver regjering og makthaver. Amnesty har utallige resultater å vise til. Hvis man vil lese mer om Amnesty kan man gå til deres nettsider.',
     image: require('./../img/images/y2015.jpeg')
   },
   {
     title: '2014 - UNICEF',
+    text:
+      'Det første året valgte vi å sette fokus på barns rettigheter. Vi samlet inn kr 1 200 til UNICEF, noe vi den gangen var fornøyd med..',
     winner: 'Jakob Hardersen Prydz',
-    organization: 'UNICEF',
     donated: 1200,
     attendees: [
       'Erlend Solbakk Harbitz',
@@ -176,10 +177,9 @@ const competitions = [
       'Thomas Matthew Brown',
       'Øystein Hammersland'
     ],
-    text:
-      'Historiens første vinner av Monsrud Open ble Jakob Hardersen Prydz som gikk seirende ut i finalen mot Ferdinand Eide Rode etter at begge to gikk ubeseiret gjennom hver sin gruppe og slo henholdsvis Sondre Angard Siljehaug og Thomas Matthew Brown 2-1 i sett i hver sin semifinale. Jakob ble dermed første person til å ta med seg vandretrofeet hjem til odel og eie frem til neste års turnering.',
     tournamentText:
-      'Denne første offisielle turneringen ble vunnet av Jakob Hardersen Prydz. I finalen slo han forhåndsfavoritten Ferdinand Eide Rode noe overraskende for mange. Hjemmehåpet Marius Monsrud skuffet og røk ut allerede i gruppespillet etter tap mot noe svakere rangerte Thomas Matthew Brown. Han tapte også en svært jevn kamp mot vinnende Jakob Hardersen Prydz. ',
+      'Historiens første vinner av Monsrud Open ble Jakob Hardersen Prydz som gikk seirende ut i finalen mot Ferdinand Eide Rode etter at begge to gikk ubeseiret gjennom hver sin gruppe og slo henholdsvis Sondre Angard Siljehaug og Thomas Matthew Brown 2-1 i sett i hver sin semifinale. Jakob ble dermed første person til å ta med seg vandretrofeet hjem til odel og eie frem til neste års turnering.',
+    organization: 'UNICEF',
     organizationInfo:
       'Det første året valgte vi å sette fokus på barns rettigheter. Vi samlet inn kr 1 200 til UNICEF noe vi den gangen var fornøyd med. UNICEF er verdens største hjelpeorganisasjon, og de jobber for barns rettigheter i over 190 land. Viktige temaer er utdanning, nødhjelp, livreddende ernæring, rent vann, vaksiner og beskyttelse. UNICEF ble startet i 1946 for hjelpe barn etter krigen. Etter hvert har arbeidet dreid seg mye om barn i Afrika, Asia og Latin-Amerika. I 1965 vant UNICEF Nobels Fredspris.',
     image: require('./../img/images/y2014.jpeg')
