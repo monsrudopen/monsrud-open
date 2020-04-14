17a5da876275930f71916f6dae3d61098464f920
Erlend Solbakk Harbitz <erlend.solbakk.harbitz@sintef.no>
2020-04-14 20:28:42 +0200

    install image carousel

--- src/components/ImageCarousel/ImageCarousel.js
+++ src/components/ImageCarousel/ImageCarousel.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import styles from './ImageCarousel.module.css';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 const ImageCarousel = () => {
   return <div>Hei</div>;
